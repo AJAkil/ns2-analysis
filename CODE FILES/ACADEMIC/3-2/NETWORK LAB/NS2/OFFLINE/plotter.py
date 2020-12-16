eb5ea4638cee22e6f2db8f21b269bc5ca8fe6c17
AJAkil <cyclosis47@gmail.com>
2020-12-17 02:10:22 +0600

    edited plotter.py

--- CODE FILES/ACADEMIC/3-2/NETWORK LAB/NS2/OFFLINE/plotter.py
+++ CODE FILES/ACADEMIC/3-2/NETWORK LAB/NS2/OFFLINE/plotter.py
@@ -28,16 +28,27 @@ def plot_graph(y_values, x_values, x_label, y_label, fig_name) -> None:
     plt.plot(x_values, y_values, '-o')
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    plt.savefig('./figures/node_variation/'+fig_name)    
+    plt.savefig(f'./figures/{x_label}_variation/'+fig_name)    
     plt.close()
 
 
-def main(x_axis) -> None:
-    plot_graph([float(thr) for thr in throughput], x_axis, 'nodes', 'throughput', 'throughput.jpg')
-    plot_graph([float(delay) for delay in avg_delay], x_axis, 'nodes', 'average delay', 'average_delay.jpg')
-    plot_graph([float(delv_rat) for delv_rat in delivery_ratio], x_axis, 'nodes', 'delivery ratio', 'delivery_ratio.jpg')
-    plot_graph([float(drop_rat) for drop_rat in drop_ratio], x_axis, 'nodes', 'drop ratio', 'drop_ratio.jpg')
+def main(x_axis, x_label) -> None:
+    plot_graph([float(thr) for thr in throughput], x_axis, x_label, 'throughput', 'throughput.jpg')
+    plot_graph([float(delay) for delay in avg_delay], x_axis, x_label, 'average delay', 'average_delay.jpg')
+    plot_graph([float(delv_rat) for delv_rat in delivery_ratio], x_axis, x_label, 'delivery ratio', 'delivery_ratio.jpg')
+    plot_graph([float(drop_rat) for drop_rat in drop_ratio], x_axis, x_label, 'drop ratio', 'drop_ratio.jpg')
 
 if __name__ == '__main__':
-    main()    
+    
+    type = input('Enter the type: ')
+    x_axis , x_labels = '',''
+    
+    if type == 'nodes':
+        x_axis, x_labels = nodes, 'nodes'
+    elif type == 'areas':
+        x_axis, x_labels = areas, 'area'
+    elif type == 'flows':
+        x_axis, x_labels = flows, 'flows'
+    
+    main(x_axis, x_labels)    
     
